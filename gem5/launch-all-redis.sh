3cda9000b6d2d934130e1bfdc7dfe9a558f83f91
Miguel <miavgu@inf.upv.es>
2021-08-06 22:20:15 +0000

    Scripts para el lanzamiento de las simulaciones

---
+++ gem5/launch-all-redis.sh
@@ -0,0 +1,8 @@
+date > incio_all_redis.txt
+./launch-frfcfs1k.sh
+./launch-frfcfscache1k.sh
+./launch-frfcfs25k.sh
+./launch-frfcfscache25k.sh
+./launch-frfcfs50k.sh
+./launch-frfcfscache50k.sh
+date > fin_all_redis.txt
---
+++ gem5/launch-frfcfscache50k.sh
@@ -0,0 +1,3 @@
+date > inicio_frfcfscache50k.txt
+build/X86/gem5.opt --outdir=frfcfscache-trace50k configs/example/fs.py --mem-type=NVMainMemory --nvmain-config=../NVmain/Config/NVM_2CH_FRFCFS-CACHE.config --cpu-type=TimingSimpleCPU --kernel=/fs-stuff/binaries/x86_64-vmlinux-4.19.83 --disk-image=/fs-stuff/disks/parsec.img --caches --l1i_size=32kB --l1i_assoc=8 --l1d_size=32kB --l1d_assoc=8 --l2cache --l2_size=256kB --l2_assoc=8 --script=scripts/redis-bm-50k-g-s-lpu-lpo.sh > stats_nvm_frfcfs_cache50k.txt
+date > fin_frfcfscache50k.txt
